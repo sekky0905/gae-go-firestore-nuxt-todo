5da7ead1e52560afc5aa189d379118dc57b3add8
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-10-29 00:43:19 +0900

    add NewTaskRepository to generate task repository

--- server/infra/db/task.go
+++ server/infra/db/task.go
@@ -1,7 +1,28 @@
 package db
 
-import "cloud.google.com/go/firestore"
+import (
+	"context"
+	"os"
+
+	"golang.org/x/xerrors"
+
+	"cloud.google.com/go/firestore"
+	"github.com/sekky0905/gae-go-firestore-nuxt-todo/domain/repository"
+)
 
 type taskRepository struct {
-	client firestore.Client
+	client *firestore.Client
+}
+
+func NewTaskRepository(ctx context.Context) (repository.TaskRepository, error) {
+	projectID := os.Getenv("PROJECT_ID")
+	if projectID == "" {
+		return nil, xerrors.New("error in main method")
+	}
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, xerrors.Errorf("new firestore client: %w", err)
+	}
+
+	return &taskRepository{client: client}, nil
 }
